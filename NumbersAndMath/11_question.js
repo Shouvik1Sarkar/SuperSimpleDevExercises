e3df454a58d14fe7fa987a08eb7f37e4caf275c6
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 12:47:52 +0530

    Question 12 13 14 done

--- NumbersAndMath/11_question.js
+++ NumbersAndMath/11_question.js
@@ -1,3 +1,3 @@
-// question-10:  Always want to round a number 2.1 to 3 how?
+// question-11:  Always want to round a number 2.1 to 3 how?
 
 console.log(Math.ceil(2.1));
---
+++ NumbersAndMath/12_question.js
@@ -0,0 +1,21 @@
+// Temperature convert
+
+// F = (C * (9 / 5)) + 32
+// C = (F - 32) * (5 / 9);
+
+//Question-12:  25 C to F
+
+let c = 25;
+let f = c * (9 / 5) + 32;
+console.log(f);
+
+//Question-13:  86 F to C
+let f1 = 86;
+let c1 = (f1 - 32) * (5 / 9);
+console.log(c1);
+
+//Question-14:  25 C to F
+
+let c2 = -5;
+let f2 = c2 * (9 / 5) + 32;
+console.log(f2);
