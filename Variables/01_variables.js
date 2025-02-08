011f2ac725ef1af0ac0aee0045575bbde7ae8af3
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-09 01:00:37 +0530

    variable 1-6 are done

---
+++ Variables/01_variables.js
@@ -0,0 +1,24 @@
+//Question-1: Sve your name in name variable
+let name = "Shouvik";
+
+//Question-2: Print name with String interpolation
+console.log(`My name is ${name}`);
+
+//Question-3: 1 coffee: $5, 2 bagels: $3 * 2, 1 soup: $9
+
+let cost = 5 + 3 * 2 + 9;
+
+//Question-4: Print  cost with String interpolation
+
+console.log(`Cost of the foos: $${cost}`);
+
+//Question-5: 10% tax
+
+let tax = cost * 0.1;
+//Question-6: display tax
+console.log(`Tax(10%): $${tax}`);
+
+//Question-7: total cost
+let total_cost = cost + tax;
+
+console.log(`Total Cost: $${total_cost}`);
