f8740762b3e560a39bf9a973c27e8f555318db3a
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 13:30:24 +0530

    String 4 5 6 done

---
+++ Strings/04_string.js
@@ -0,0 +1,15 @@
+// Question-4: 1 coffe: 5 1 bagel: 3. total cost. concat "Total cost $__"
+
+let line = "Total cost $";
+
+let coffee = 5;
+let bagel = 3;
+let total = coffee + bagel;
+console.log(line.concat(total, "."));
+
+// Question-5: String interpolation
+console.log(`Total cost $${total}.`);
+
+// Question-6: Display in alert
+
+// alert(`Total cost $${total}.`);
