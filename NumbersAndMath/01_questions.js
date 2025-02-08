d8fe14c6c4504e79bfde872705febab28bf6c67a
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 10:52:03 +0530

    First question done

---
+++ NumbersAndMath/01_questions.js
@@ -0,0 +1,9 @@
+// Question-1: bought 1 soup for 30$, 3 Burgers for 8$ each, 1 icecream for 5$. How much in total.
+
+let soup = 30;
+let burger = 8 * 3;
+let icecream = 5;
+
+let total_cost = soup + burger + icecream;
+
+console.log(total_cost);
