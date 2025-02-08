c0d58ddf66236e358fc249255fef102fa1f77bbf
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 11:02:48 +0530

    Fourth question done

---
+++ NumbersAndMath/04_question.js
@@ -0,0 +1,6 @@
+// Question-4: toaster($18.50) and 2 T-shirts ($7.50 each). Calculte 10% Tax.
+
+let toaster = 18.5;
+let tShirt = 7.5;
+let tax_amount = (toaster + tShirt * 2) * (10 / 100);
+console.log(tax_amount); // Output: 3.35
