c6e1bdb912e70ff8172e5921ae6225ff3059f391
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 13:13:52 +0530

    updated 3

--- Strings/01_string.js
+++ Strings/01_string.js
@@ -1,5 +1,6 @@
 // Question-1: Create the text  "My name is: " as a string.
 // Question-2: Create your name as a string.
+// Question-3: concat two
 let var1 = "My name is:";
 let name = "Shouvik";
 console.log(var1.concat(name));
