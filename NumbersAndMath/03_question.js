126d38ca17a00bf4fdb0004d09fb27df2d652d45
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-08 10:59:24 +0530

    Third question done

---
+++ NumbersAndMath/03_question.js
@@ -0,0 +1,6 @@
+// Question-3: Total cost of a toaster($18.50) and 2 T-shirts ($7.50 each)
+
+let toaster = 18.5;
+let tShirt = 7.5;
+let totalCost = toaster + tShirt * 2;
+console.log(totalCost); // Output: 33.5
